86ce5decc6a1374683328e421e6ca92b2b7d3f77
ytrofnom <ysabelmonfort@gmail.com>
2017-12-29 15:04:26 +0100

    Using toggleClass instead of add/removeClass.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,10 @@
 $(document).ready(() => {
-
   $('.login-button').on('click', () => {
-    $('.login-form').toggle();
+    $('.login-form').toggleClass('hide');
   });
 
-  $('.menu-button').on('mouseenter', () => {
-    $('.nav-menu').removeClass('hide');
-    $('.menu-button').addClass('button-active');
+  $('.menu-button').on('click', () => {
+    $('.nav-menu').toggleClass('hide');
+    $('.menu-button').toggleClass('button-active');
   })
-
-  $('.nav-menu').on('mouseleave', () => {
-    $('.nav-menu').hide();
-    $('.menu-button').animate({
-      color: '#EFEFEF',
-      backgroundColor: '#303030',
-      fontSize: '18px'
-    }, 200);
-  })
-
 }); 
